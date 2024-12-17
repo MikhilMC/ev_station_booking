f89e02eb6c47124a8857d3d5e23e54f7a33e9e07
MikhilMC <mikhilmohan13@gmail.com>
2024-12-17 14:42:47 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:ev_station_booking/views/on_boarding_widget.dart';
+import 'package:flutter/material.dart';
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() {
+  // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+        ),
+        useMaterial3: true,
+      ),
+      home: const OnBoardingWidget(),
+    );
+  }
+}
